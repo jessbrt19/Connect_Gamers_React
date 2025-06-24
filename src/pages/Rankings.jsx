85977007edc6e316d0caed42a76001890779c2db
Jéssica Brito <jessicabrito0619@gmail.com>
2025-06-24 17:16:11 -0300

    Update Rankings.jsx

--- src/pages/Rankings.jsx
+++ src/pages/Rankings.jsx
@@ -1,14 +1,4 @@
-// src/pages/Home.jsx
-import { useTheme } from "../theme/ThemeContext";
 
-export default function Rankings() {
-  const { theme } = useTheme();
-  return (
-    <div className={`text-center mt-10 ${theme === "dark" ? "text-white" : "text-black"}`}>
-      <h1 className="text-3xl font-bold">Página Rankings</h1>
-    </div>
-  );
-}
 import React, { useState } from 'react';
 
 const Rankings = () => {
